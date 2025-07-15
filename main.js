f0bf7811ce4cb66837f5adf0d305ad9fbbf80231
Re1zz <salauatkaisar@gmail.com>
2025-07-15 13:57:56 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const menuButtom = document.querySelector('.menu_button');
+const menu = document.querySelector('.menu');
+
+menuButtom.addEventListener('click', () =>{
+    menu.classList.toggle('menu--open')
+});
+
+
